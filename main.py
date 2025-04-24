479235432bb52bfa8420f7dbb14092645613b6f8
acuev28 <alejandrocuevas188@gmail.com>
2025-04-24 14:45:57 -0500

    Added the main and rule-based function

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import eliza 
+import re
 import pandas as pd
 import numpy as np
 from datetime import datetime
-from sentence_transformers import sentenceTransformer, util
+from sentence_transformers import SentenceTransformer, util
+
+def rule_based():
+    eliza = eliza.Eliza() # load the model
+
+
+
+def main():
+    print("What type of chatbot would you like to chat with?\n")
+    print("1: ELIZA (Rule-based)\n2: Corpus Based\n")
+
+    user_choice = input("Enter your chatbot choice: ")
+
+
+
+
+main()
