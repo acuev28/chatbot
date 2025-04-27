ec5dfab23e93b4eb6f34a8d9b3d1b33c74abddd6
acuev28 <alejandrocuevas188@gmail.com>
2025-04-26 19:52:43 -0500

    Finished rule-based (eliza) function

--- main.py
+++ main.py
@@ -7,23 +7,38 @@ from sentence_transformers import SentenceTransformer, util
 
 def is_end(input_string):
     # This will check if the user wants to end conversation with the chatbot
-    pattern = re.compile(r'\(bye|exit)\b', re.IGNORECASE)
+    pattern = re.compile(r'(bye|exit)\b', re.IGNORECASE)
     if pattern.search(input_string):
         return True
     return False
 
-def rule_based():
-    eliza = eliza.Eliza() # load the model
 
+def rule_based():
+    eliza_chat = eliza.Eliza() # load the model
+
+    # Start the chat
+    while True:
+        user_input = input("\nYou: ")
+
+        # print(f"\nUser: {user_input}")
+
+        # If the user wants to end chat by typing ("bye" or "exit")
+        if is_end(user_input):
+            print("Agent: Goodbye!")
+            break
+        # Get ELIZA response
+        eliza_response = eliza_chat.respond(user_input)
+        print(f"Agent: {eliza_response}")
 
 
 def main():
     print("What type of chatbot would you like to chat with?\n")
-    print("1: ELIZA (Rule-based)\n2: Corpus Based\n")
+    print("1: ELIZA (Rule-based)")
+    print("2: Corpus Based")
 
     user_choice = input("Enter your chatbot choice: ")
 
-
-
+    if (user_choice == "1"):
+        rule_based()
 
 main()
